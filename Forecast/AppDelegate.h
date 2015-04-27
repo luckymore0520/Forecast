3756b92ab6bdccda776ec7d94b7628f29dffbde2
luck-mac <luck-mac@luck-macdeMacBook-Air.local>
2015-04-27 20:11:03 +0800

    Initial Commit

---
+++ Forecast/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Forecast
+//
+//  Created by luck-mac on 15/4/27.
+//  Copyright (c) 2015年 com.nju.luckymore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ Forecast/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Forecast
+//
+//  Created by luck-mac on 15/4/27.
+//  Copyright (c) 2015年 com.nju.luckymore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
